d49c6c7fd92c32017e3089d8fe871b55dd44fcb5
tuyishimejohnson <j.tuyishime4@alustudent.com>
2023-12-29 22:11:49 +0200

    Some changes to the platform.

---
+++ ivuuze_folder/scripts.js
@@ -0,0 +1,3 @@
+document.querySelector(".more-videos").addEventListener("click", function otherTutorials() {
+    window.location.href = "https://www.youtube.com/results?search_query=guteka+indyo+yuzuye";
+});
