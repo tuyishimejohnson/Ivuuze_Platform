a8c1371eaaa236efa7b7b53c9d29ecf5821738fa
tuyishimejohnson <j.tuyishime4@alustudent.com>
2023-12-31 13:10:40 +0200

    Adding videos and styling them.

--- ivuuze_folder/scripts.js
+++
@@ -1,3 +0,0 @@
-document.querySelector(".more-videos").addEventListener("click", function otherTutorials() {
-    window.location.href = "https://www.youtube.com/results?search_query=guteka+indyo+yuzuye";
-});
